a078ad9387bd396423dab088e6e8cbd4193b0ad5
Dmitry Perepelov <d.perepelov999@gmail.com>
2022-07-28 22:01:19 +0300

    Add project files.

---
+++ Summator/Interface.cs
@@ -0,0 +1,7 @@
+﻿namespace Summator
+{
+    public interface ISum
+    {
+        int Sum(int a, int b);
+    }
+}
---
+++ Summator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите первое число: ");
+                var int1 = int.Parse(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                var int2 = int.Parse(Console.ReadLine());
+
+                var summator = new Summator();
+
+                Console.WriteLine($"Сумма: {summator.Sum(int1, int2)}");
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Summator : ISum
+    {
+        public int Sum(int a, int b)
+        {
+            return checked(a + b);
+        }
+    }
+}
